96cc33154020837450c1e242fdcfbaee0b1de2ee
Ishaan Bansal <35862574+bshaan77@users.noreply.github.com>
2023-07-27 16:29:28 -0700

    update prod and test env

--- bot/src/env.d.ts
+++ bot/src/env.d.ts
@@ -5,8 +5,7 @@ declare global {
       DISCORD_BOT_INTENT_EVERYTHING: string;
       DISCORD_BOT_GUILD_ID: string;
       DISCORD_BOT_CHANNEL_ID: string;
-      TEST_BACKEND_URL: string;
-      PROD_BACKEND_URL: string;
+      BACKEND_URL: string;
     }
   }
 }
