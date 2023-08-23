f700131bca678c5f6f57f54ce7a308fe642b34aa
Ethan Kim <ethankim8683@gmail.com>
2023-08-22 21:34:28 -0700

    Trying to get the hang of merges
    Was I? I was too lazy to bathe
    Or paint or write or try to make a change
    Now I can shoot a gun to kill my lunch
    And I don't have to love or think too much

--- bot/src/constants.ts
+++
@@ -1,4 +0,0 @@
-export const SMS_PLATFORM = 1;
-export const DISCORD_PLATFORM = 2;
-
-export const STUDENT_ROLE = 3;
--- bot/src/utils/validate.ts
+++
@@ -1,7 +0,0 @@
-export function validateName(name: string) {
-  return /^\w+$/.test(name);
-}
-
-export function validatePhoneNumber(phoneNumber: string) {
-  return /^1?\d{10}$/.test(phoneNumber);
-}
